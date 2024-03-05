205fa53fc1701d4f1320f8068b8864ff54a9dcdb
Dani <d.iwannikow@gmail.com>
2024-03-05 13:56:51 +0100

    Leeres Projekt

---
+++ src/main/java/com/ap/DFormsApplication.java
@@ -0,0 +1,13 @@
+package com.ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DFormsApplication.class, args);
+	}
+
+}
